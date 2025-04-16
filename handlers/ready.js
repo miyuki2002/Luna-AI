e5f59b337d05f5ee692f775dcd91aeefc1f67d28
s4ory <hoanghcm188@gmail.com>
2025-04-16 09:29:12 +0700

    refactor: clean up console logs in ready handler

--- handlers/ready.js
+++ handlers/ready.js
@@ -2,7 +2,6 @@ const grokClient = require('../services/grokClient');
 
 function handleReady(client, loadCommands) {
   client.once('ready', async () => {
-    console.log('\x1b[33m%s\x1b[0m', `***************************************\n`);
     console.log('\x1b[36m%s\x1b[0m', `
     ██╗     ██╗   ██╗███╗   ██╗ █████╗ 
     ██║     ██║   ██║████╗  ██║██╔══██╗
@@ -11,12 +10,7 @@ function handleReady(client, loadCommands) {
     ███████╗╚██████╔╝██║ ╚████║██║  ██║
     ╚══════╝ ╚═════╝ ╚═╝  ╚═══╝╚═╝  ╚═╝
     `);
-    
-    console.log('\x1b[33m%s\x1b[0m', `*** Discord Bot Online - Version 1.0.0 ***`);
-    console.log('\x1b[33m%s\x1b[0m', `*** Logged in as: ${client.user.tag} ***`);
-    console.log('\x1b[33m%s\x1b[0m', `*** Bot is ready to assist! ***`);
-    console.log('\x1b[33m%s\x1b[0m', `***************************************\n`);
-    
+
     // Tải các lệnh khi khởi động
     const commandCount = loadCommands(client);
     console.log('\x1b[32m%s\x1b[0m', `Đã tải tổng cộng ${commandCount} lệnh!`);
