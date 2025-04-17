172617626df9072c4b74751164270574c8e5bdc2
s4ory <hoanghcm188@gmail.com>
2025-04-17 13:52:44 +0700

    Prettier

--- commands/reset.js
+++ commands/reset.js
@@ -5,7 +5,7 @@ module.exports = {
   data: new SlashCommandBuilder()
     .setName('reset')
     .setDescription('Đặt lại cuộc trò chuyện với bot'),
-  
+
   async execute(interaction) {
     await storageDB.clearConversationHistory(message.author.id, this.systemPrompt, this.Model);
     interaction.reply({ content: 'Cuộc trò chuyện đã được đặt lại!', ephemeral: true });
